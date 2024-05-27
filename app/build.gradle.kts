f9be911b643211e167447f3460eff50585dd2886
ibnuputraard <ibnu.putra@nawadata.id>
2024-05-27 16:52:15 +0700

    update logo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.ardclient.esikap"
         minSdk = 24
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
