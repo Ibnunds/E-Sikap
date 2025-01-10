bab61662fef1e5692e4625a1d9a6795b5e92693b
ibnuputraard <ibnu.putra@nawadata.id>
2025-01-10 13:06:10 +0700

    build release

--- app/src/main/java/com/ardclient/esikap/service/ApiClient.kt
+++ app/src/main/java/com/ardclient/esikap/service/ApiClient.kt
@@ -11,12 +11,12 @@ val okHttpClient: OkHttpClient? = OkHttpClient.Builder()
     .connectTimeout(300, TimeUnit.SECONDS)
     .build()
 object RetrofitClient {
-    private const val BASE_URL = "http://10.0.2.2:3001/" //"http://10.0.2.2:3001/" //"https://esikapdev.my.id/"
+    private const val BASE_URL = "https://esikapdev.my.id/" //"http://10.0.2.2:3001/" //"https://esikapdev.my.id/"
 
     val retrofit: Retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())git 
             .client(okHttpClient!!)
             .build()
     }
